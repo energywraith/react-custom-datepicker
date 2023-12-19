f0c64aa2fa8106cef149ca92540fbb28018451d0
energywraith <energywraith@gmail.com>
2023-12-19 17:15:56 +0100

    feat: Add TypeScript

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,3 +1,27 @@
-const DatePicker = () => {};
+import { useState } from "react";
+
+type Props = {
+  value?: number;
+};
+
+const DatePicker = ({ value = 0 }: Props) => {
+  const [counter, setCounter] = useState(value);
+
+  const onMinus = () => {
+    setCounter((prev) => prev - 1);
+  };
+
+  const onPlus = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={onMinus}>-</button>
+      <button onClick={onPlus}>+</button>
+    </div>
+  );
+};
 
 export { DatePicker };
